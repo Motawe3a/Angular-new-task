4fd3890ad85e21b44701b2caf41d6828a95ae297
motawe3 <Amotaweaa@tafeel.com>
2019-09-28 11:28:23 +0200

    handle error

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,40 +1,61 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { IProducts, IOrders} from './dataInterfaces';
 import { Observable } from 'rxjs/internal/Observable';
-import { map } from 'rxjs/operators';
+import { map, catchError, retry } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
 
-  constructor(private _http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   getAllProducts(): Observable<IProducts[]> {
-    return this._http.get<IProducts[]>('./assets/data/Products.json')
+    return this.http.get<IProducts[]>('./assets/data/Products.json', {responseType: 'json'})
+    .pipe(
+      retry(2),
+      catchError(this.handleError)
+    );
   }
 
   getOrders(): Observable<IOrders[]> {
-    const requestOptions = Object.assign(
-      { responseType: 'json' },
+
+    return this.http.get<IOrders[]>('./assets/data/Orders.json')
+    .pipe(
+      retry(2),
+      catchError(this.handleError)
     );
-    return this._http.get<IOrders[]>('./assets/data/Orders.json');
   }
 
   getproduct(id: number): Observable<IProducts[]> {
-    return this._http.get<IProducts[]>('./assets/data/Products.json').pipe(map((value) => {
-        return value.filter(ele => {
-          return ele.ProductId === id;
-        });
-    }));
+    return this.http.get<IProducts[]>('./assets/data/Products.json').pipe(
+
+        retry(2),
+        map((value) => {
+          return value.filter(ele => {
+            return ele.ProductId === id;
+          });
+        }),
+        catchError(this.handleError)
+        );
   }
 
   getOrder(id: number): Observable<IOrders[]> {
-    return this._http.get<IOrders[]>('./assets/data/Orders.json').pipe(map((value) => {
-        return value.filter(ele => {
-          return ele.OrderId === id;
-        });
-    }));
+    return this.http.get<IOrders[]>('./assets/data/Orders.json').pipe(
+        retry(2),
+        map((value) => {
+          return value.filter(ele => {
+            return ele.OrderId === id;
+          });
+        }),
+        catchError(this.handleError)
+    );
+  }
+
+  handleError(error: HttpErrorResponse) {
+    //add any kind of message to return
+    return throwError(error);
   }
 }
